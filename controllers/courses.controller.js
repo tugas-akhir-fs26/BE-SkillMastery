bd40ed317c1c7f4dd3a87a4a9a5ad7b988079bc1
Dionisius951 <dionisius951@gmail.com>
2023-11-28 22:59:22 +0700

    menambahkan endpoint get course by id

--- controllers/courses.controller.js
+++ controllers/courses.controller.js
@@ -1,17 +1,39 @@
 // @ts-nocheck
-const {Course} = require('../models')
+const { Course } = require("../models");
 
 module.exports = {
-    getCourses : async (req, res) => {
-        try {
-            const data = await Course.findAll({ limit: 8 });
-            
-            res.status(200).json({
-                ok : true,
-                data : data
-            })
-        } catch(error) {
-            console.error(error);
-        }
+  getCourses: async (req, res) => {
+    try {
+      const data = await Course.findAll({ limit: 4 });
+      if (data.length > 0) {
+        res.status(200).json({
+          ok: true,
+          data: data,
+        });
+      }
+      
+    } catch (error) {
+      console.error(error);
     }
-}
+  },
+  getCoursesByID: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const data = await Course.findAll({ where: { id: id } });
+
+      if (data.length > 0) {
+        res.status(200).json({
+          ok: true,
+          data: data,
+        });
+      }
+
+      res.status(400).json({
+        ok: false,
+        message: "Error saat pengambilan data!",
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
--- routes/courses.route.js
+++ routes/courses.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const {getCourses} = require('../controllers/courses.controller')
+const {getCourses, getCoursesByID} = require('../controllers/courses.controller')
 const route = express.Router()
 
 
 route.get("/", getCourses)
+route.get("/:id", getCoursesByID)
 
 
 module.exports = route
