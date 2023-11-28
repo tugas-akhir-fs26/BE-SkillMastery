54903f23e374e2bae674f3c2e28cfdaedc330cee
Dionisius951 <dionisius951@gmail.com>
2023-11-28 23:55:21 +0700

    fix config.js

--- config/config.js
+++ config/config.js
@@ -8,17 +8,19 @@ module.exports = {
     "port" : 3306
   },
   "test": {
-    "username": "freedb_skillmastery",
-    "password": "eSPjd&X8tB3$?Fs",
-    "database": "freedb_SkillMastery",
-    "host": "sql.freedb.tech",
-    "dialect": "mysql"
+    "username": "sql12665711",
+    "password": "Bxqy9weyCm",
+    "database": "sql12665711",
+    "host": "sql12.freesqldatabase.com",
+    "dialect": "mysql",
+    "port" : 3306
   },
   "production": {
-    "username": "freedb_skillmastery",
-    "password": "eSPjd&X8tB3$?Fs",
-    "database": "freedb_SkillMastery",
-    "host": "sql.freedb.tech",
-    "dialect": "mysql"
+    "username": "sql12665711",
+    "password": "Bxqy9weyCm",
+    "database": "sql12665711",
+    "host": "sql12.freesqldatabase.com",
+    "dialect": "mysql",
+    "port" : 3306
   }
 }
